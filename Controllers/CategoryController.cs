01f1cf5e3a5ebd89d43191beeba5c62dcf4db096
oaltay <ozgenur12331@gmail.com>
2023-01-18 21:20:20 +0300

    Initial commit

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.UpSchool.DAL;
+using WebApi.UpSchool.DAL.Entities;
+
+namespace WebApi.UpSchool.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        Context context = new Context();
+
+        [HttpGet]
+        public IActionResult CategoryList()
+        {
+            var categories = context.Categories.ToList();
+            return Ok(categories);
+        }
+
+        [HttpPost]
+        public IActionResult CategoryAdd(Category category)
+        {
+            context.Categories.Add(category);
+            context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult CategoryGet(int id)
+        {
+            var category = context.Categories.Find(id);
+            return Ok(category);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult CatgoryDelete(int id)
+        {
+            var category = context.Categories.Find(id);
+            context.Categories.Remove(category);
+            context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult CategoryUpdate(Category category)
+        {
+            var categoryUpdate = context.Categories.Find(category.CategoryID);
+            categoryUpdate.CategoryName = category.CategoryName;
+            categoryUpdate.Description = category.Description;
+            categoryUpdate.Status = category.Status;
+            context.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ DAL/Context.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.UpSchool.DAL.Entities;
+
+namespace WebApi.UpSchool.DAL
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=LAPTOP-8GBIAO0U\\SQLEXPRESS;Database=DbWebApi;integrated security=true");
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
